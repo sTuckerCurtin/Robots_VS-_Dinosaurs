e04d3f8d6f053af94bdaab94558d43fd419f218d
Samuel Curtin <stuckercurtin@gmail.com>
2023-03-31 12:54:51 -0400

    Dinosaur class created

---
+++ dinosaur.py
@@ -0,0 +1,9 @@
+class Dinosaur:
+    def __init__(self, name, attack_power) -> None:
+        self.name = name
+        self.attack_power = attack_power
+        self.health = 100
+
+
+    def attack(self, robot):
+        pass
