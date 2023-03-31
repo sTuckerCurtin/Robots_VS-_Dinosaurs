54aae837e7a4686ab4f812323cfc650f990bcf1a
Samuel Curtin <stuckercurtin@gmail.com>
2023-03-31 12:50:33 -0400

    Robot Class created

---
+++ battlefield.py
@@ -0,0 +1,21 @@
+class Battlefield:
+    def __init__(self):
+        self.robot = ""
+        self.dinosaur = ""
+
+
+    def run_game(self):
+        pass
+
+
+    def display_welcome(self):
+        pass
+
+
+    def battle_phase(self):
+        pass
+
+
+    def display_winner(self):
+        pass
+        
---
+++ robot.py
@@ -0,0 +1,9 @@
+class Robot:
+    def __init__(self, name,):
+        self.name = name
+        self.health = 100
+        self.active_weapon = ""
+
+    def attack(self, dinosaur):
+        pass
+        
