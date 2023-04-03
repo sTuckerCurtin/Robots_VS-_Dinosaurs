cb47fff32ed4823ab24f85dcb25f8aa9cd36bb28
Samuel Curtin <stuckercurtin@gmail.com>
2023-04-03 10:21:34 -0400

    FINAL MVP

--- battlefield.py
+++ battlefield.py
@@ -21,7 +21,7 @@ class Battlefield:
 
     def display_welcome(self):
         print()
-        print("In year 2077. Dr. Sam Curtin discovered a fully preserved Dinosaur under a sheet of ice in the Arctic Circle, thats been melted away by climate change. While the Dinosaur was being studied it woke up and started charging toward Northern Canada ! With the Dinosaur terrorizing the Canucks, mankind built the ultimate weapon to fight this beast...his name was .....RoBob  ")
+        print("Welcome to the Murderdome! 2 fighters will enter......1 will emerge a Champion! ")
         print()
 
 
@@ -41,10 +41,10 @@ class Battlefield:
     def display_winner(self):
         if self.dinosaur.health > self.robot.health:
             print()
-            print(f"All Hail {self.dinosaur.name}, The new King of The World....Humanity will become extinct and the era of the Dinosaur will start again ")
+            print(f"All Hail {self.dinosaur.name}, The new King of The World!")
         elif self.robot.health > self.dinosaur.health:
             print()
-            print(f"{self.robot.name} has won! Humanity is saved!")
+            print(f"{self.robot.name} has won! The NEW Champion of the World !")
         else:
             print("It's a tie!")
 
